e337882a3c8559483a7f1c3c0cb3adc246a1a84a
Ugur Celik <ucelik.dev@gmail.com>
2023-11-12 01:47:33 +0100

    update buttons

--- app/products/[id]/EditProductButton.tsx
+++ app/products/[id]/EditProductButton.tsx
@@ -4,9 +4,8 @@ import Link from 'next/link';
 const EditProductButton = ({ productId }: { productId: number }) => {
   return (
     <Link href={`/products/edit/${productId}`}>
-      <button className=' bg-blue-600 text-white hover:cursor-pointer' >
-        <BsPencilSquare />
-        Edit
+      <button type="button" className="hover:cursor-pointer text-white bg-blue-600 hover:bg-blue-700  font-medium rounded-md text-sm px-4 py-1.5 text-center inline-flex gap-2 items-center me-2 mb-2">
+        <BsPencilSquare/>Edit
       </button>
     </Link>
   );
